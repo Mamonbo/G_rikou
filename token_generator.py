b3c04905c9ca060f1537895b8af14c37a96f8c47
Mamonbo <9661031@gmail.com>
2017-08-27 18:14:42 +0900

    トークン生成部分を実装

--- token_generator.py
+++ token_generator.py
@@ -1,5 +1,6 @@
 #coding=UTF-8
 import json
+import mastodon
 
 FilePointer=open('user.json','r')
 
@@ -7,5 +8,26 @@ FilePointer=open('user.json','r')
 
 
 Setting=json.load(FilePointer)
-print(Setting)
+#print(Setting)
 FilePointer.close()
+
+# Register
+# インスタンス毎に必要
+mastodon.Mastodon.create_app(
+    'global.rikou.don',
+    api_base_url = Setting['url'],
+    to_file = 'client_cred.txt'
+)
+
+# Login
+# ユーザー毎のトークンも生成する
+mstdn_handler=mastodon.Mastodon(
+    client_id = 'client_cred.txt',
+    api_base_url = Setting['url']
+)
+
+mstdn_handler.log_in(
+    Setting['usermail'],
+    Setting['password'],
+    to_file = 'user_cred.txt'
+)
